cafe201c90cdffed220e5f710cda52b71b2c3ce5
herdo dimas pratirto <71863608+herdodp22@users.noreply.github.com>
2022-06-06 14:50:02 +0700

    function parameter

---
+++ functionparameter.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func penjumlahan(angka1 int, angka2 int) (hasil int) {
+	hasil = angka1 + angka2
+	return
+}
+
+func main() {
+
+	fmt.Print(penjumlahan(10, 10))
+
+}
