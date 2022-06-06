e90e5a028fbe0ef2e78175583715649be74e5137
herdo dimas pratirto <71863608+herdodp22@users.noreply.github.com>
2022-06-06 07:03:36 +0700

    operasi matematika

---
+++ operasimatematika.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//operasi matematika
+
+	//penjumlahan
+	angka1 := 10
+	angka2 := 20
+
+	hasiljumlah := angka1 + angka2
+	hasilkali := angka1 * angka2
+	hasilbagi := angka1 / angka2
+	hasilkurang := angka1 - angka2
+
+	fmt.Println(hasiljumlah)
+	fmt.Println(hasilkali)
+	fmt.Println(hasilbagi)
+	fmt.Println(hasilkurang)
+
+}
