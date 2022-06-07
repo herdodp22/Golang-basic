c31ef51425a796d14fb751d91676e9f028fe0aec
herdo dimas pratirto <71863608+herdodp22@users.noreply.github.com>
2022-06-07 22:42:45 +0700

    Add files via upload

---
+++ ifelse.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var angka = 10
+
+	if angka >= 10 {
+		fmt.Println("angka lebih dari sama dengan 10")
+	} else {
+		fmt.Println("angka kurang dari 10")
+	}
+
+}
